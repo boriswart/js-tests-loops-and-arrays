151c70bcc317b0bb707d793c752b854abf8be3db
boriswart <boriswart@yahoo.com>
2021-05-11 14:24:23 -0600

    fixeall goodd loops...

--- app/loops.js
+++ app/loops.js
@@ -6,7 +6,12 @@
 // 1*2*3*4 = 24
 
 function factorials(num) {
-
+    let accum = 1
+    let i = 1
+    for (i = 1; i <= num; i++) {
+        accum = accum * i
+    }
+    return accum
 }
 
 // ------------------------------------------
@@ -17,18 +22,32 @@ function factorials(num) {
 // output: 20
 
 function rangeTotal(n1, n2) {
-
+    let accum = 0
+    let i = n1
+    for (i = n1; i <= n2; i++) {
+        accum += i
+    }
+    return accum
 }
 
 // ------------------------------------------
 
 
 // 3. Write a function that will take in a starting number, ending number, and a divisor. Iterate
-// over the range of the numbers and rtuen how many numbers can be divided by the divisor.
+// over the range of the numbers and rturn how many numbers can be divided by the divisor.
 // Example:
 // input: 15, 20, 5
 // output: 2
 
 function rangeDivisor(start, end, divisor) {
-
+    let accum = 0
+    let i = start
+    numOfNums = 0
+    for (i = start; i <= end; i++) {
+        if (i % divisor == 0) {
+            accum++
+        }
+    }
+    return accum
 }
+
